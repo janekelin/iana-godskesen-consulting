3ea1077dd6f77f8dc7f4fc263203c78f1136405f
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-08-20 13:41:54 +0200

    Create DOM injection skeleton

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//DOM Manipulation
 window.addEventListener("load", function(){
 	var nodes = document.querySelectorAll("#mainNav a");
 	for(var i=0; i<nodes.length; i++){
@@ -8,3 +9,31 @@ window.addEventListener("load", function(){
 		});
 	}
 });
+
+//React Injection
+// Imports
+const {Component, createElement} = React;
+
+class ProjectSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: [1],
+    };
+  }
+
+  render() {
+    if(!this.state.projects.length) {
+      return 'Loading...';
+    }
+
+    return createElement(
+      'section',
+      {id: 'projects'},
+      'Section loaded'
+    );
+  }
+}
+
+const projectSectionContainer = document.querySelector('#js-container-projects');
+ReactDOM.render(createElement(ProjectSection), projectSectionContainer);
